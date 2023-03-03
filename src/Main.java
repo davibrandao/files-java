5944f0ab76076e9ec4a66f83624ddea643c8668e
Davi Lauritzen <davi.lauritzen1@gmail.com>
2023-03-03 09:19:44 -0300

    Creating object Product

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader()))
+        {
+
+        }
+    }
+}
